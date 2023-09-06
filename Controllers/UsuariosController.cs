bb2a9780dbaabd586b2369b929d7ad02ba372d1e
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-09-06 11:39:10 +0200

    token jwt

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -8,14 +8,44 @@ using Microsoft.EntityFrameworkCore;
 using partyholic_api.Models;
 using partyholic_api.Dto;
 using System.Web.Http.Description;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace partyholic_api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class UsuariosController : ControllerBase
     {
         private readonly partyholicContext _context;
+
+        private readonly IConfiguration _configuration;
+
+        private string CreateToken(Usuario usuario)
+        {
+            
+
+        List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, usuario.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings: Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: cred
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
 
         public UsuariosController(partyholicContext context)
         {
@@ -33,26 +63,31 @@ namespace partyholic_api.Controllers
             return await _context.Usuarios.ToListAsync();
         }
 
-        // DTO  GET: api/Usuarios/auth/signin/username
+        // DTO  POST: api/Usuarios/auth/signin/username
         //@sgallego
-        [Route("auth/signin/{username}")]
-        [HttpGet]
-        public List<UsuarioAuthDto> signin(string username)
+        [Route("auth/signin")]
+        [HttpPost]
+        public IActionResult signin(Usuario usuario)
         {
-            List<UsuarioAuthDto> LstUsuAuth = new List<UsuarioAuthDto>();
-            foreach (Usuario prd in _context.Usuarios.ToList())
+            try
             {
-                if (username == prd.Username)
+                List<UsuarioAuthDto> LstUsuAuth = new List<UsuarioAuthDto>();
+                foreach (Usuario prd in _context.Usuarios.ToList())
                 {
-                    UsuarioAuthDto objdto = new UsuarioAuthDto();
-                    objdto.Username = prd.Username;
-                    objdto.Email = prd.Email;
-                    objdto.RolApp = prd.RolApp;
-                    objdto.Passwd = prd.Passwd;
-                    LstUsuAuth.Add(objdto);
+                    if (usuario.Username == prd.Username && usuario.Passwd == prd.Passwd)
+                    {
+                        //string token = CreateToken(usuario);
+
+                        return Ok(new {accessToken = "prueba", infoUser = usuario});
+
+                    }
                 }
+                return BadRequest();
             }
-            return LstUsuAuth;
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
         }
         // DTO POST
         [Route("auth/signup")]
