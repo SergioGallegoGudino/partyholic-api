6374061919965a8ba0919c376023aa3b4af293d9
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-09-12 19:27:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using partyholic_api.Models;
-using Microsoft.Extensions.Configuration;
-using System.Configuration;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,30 +11,6 @@ builder.WebHost.UseUrls($"http://*:{port};");
 
 var app = builder.Build();
 
-// Replace with your connection string.
-var connectionString = "mysql://root:YGi3MC9bN7U3JakoLQyn@containers-us-west-148.railway.app:7380/railway";
-
-// Replace with your server version and type.
-// Use 'MariaDbServerVersion' for MariaDB.
-// Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
-// For common usages, see pull request #1233.
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
-
-// Replace 'YourDbContext' with the name of your own DbContext derived class.
-builder.Services.AddDbContext<partyholicContext>(
-    dbContextOptions => dbContextOptions
-        .UseMySql(connectionString, serverVersion)
-        // The following three options help with debugging, but should
-        // be changed or removed for production.
-        .LogTo(Console.WriteLine, LogLevel.Information)
-        .EnableSensitiveDataLogging()
-        .EnableDetailedErrors()
-);
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
-builder.WebHost.UseUrls($"https://*:{port};");
-
-var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -48,12 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(x => x
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); //For development purpose, comment out the line that forces HTTPS redirection in your `Program.cs`.
 
 app.UseAuthorization();
 
