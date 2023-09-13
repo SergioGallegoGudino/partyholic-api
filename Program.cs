d40a79ec4dc6d1daabd68abb586c6fbc104dbbba
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-09-13 18:09:35 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,13 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Replace with your connection string.
-var connectionString = "server=127.0.0.1;user=root;password=root;database=partyholic";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+builder.WebHost.UseUrls($"https://*:{port};");
 
 // Replace with your server version and type.
 // Use 'MariaDbServerVersion' for MariaDB.
@@ -25,18 +24,6 @@ var connectionString = "server=127.0.0.1;user=root;password=root;database=partyh
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
 
 // Replace 'YourDbContext' with the name of your own DbContext derived class.
-builder.Services.AddDbContext<partyholicContext>(
-    dbContextOptions => dbContextOptions
-        .UseMySql(connectionString, serverVersion)
-        // The following three options help with debugging, but should
-        // be changed or removed for production.
-        .LogTo(Console.WriteLine, LogLevel.Information)
-        .EnableSensitiveDataLogging()
-        .EnableDetailedErrors()
-);
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
-builder.WebHost.UseUrls($"https://*:{port};");
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
