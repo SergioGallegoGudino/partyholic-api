0b53acbe756ef6ee78c3cb2892d0f0f49296876e
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-09-14 22:59:00 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,24 +16,7 @@ builder.Services.AddDbContext<partyholicContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connectionString = "server=containers-us-west-148.railway.app;user=root;password=YGi3MC9bN7U3JakoLQyn;database=railway";
 
-// Replace with your server version and type.
-// Use 'MariaDbServerVersion' for MariaDB.
-// Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
-// For common usages, see pull request #1233.
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
-
-// Replace 'YourDbContext' with the name of your own DbContext derived class.
-builder.Services.AddDbContext<partyholicContext>(
-    dbContextOptions => dbContextOptions
-        .UseMySql(connectionString, serverVersion)
-        // The following three options help with debugging, but should
-        // be changed or removed for production.
-        .LogTo(Console.WriteLine, LogLevel.Information)
-        .EnableSensitiveDataLogging()
-        .EnableDetailedErrors()
-);
 var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
 builder.WebHost.UseUrls($"http://*:{port};");
 
