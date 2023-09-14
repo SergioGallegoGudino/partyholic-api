91de655fc5d5bfcd7332223e67e304d6db9f8378
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-09-14 21:03:42 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<PartyholicContext>(opt =>
+    opt.UseInMemoryDatabase("Cliente"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
