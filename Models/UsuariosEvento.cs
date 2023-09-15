0c9cd7687e03613ccd3086c8b5674013dac9aa99
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-09-16 01:27:17 +0200

    prueba

--- Models/UsuariosEvento.cs
+++ Models/UsuariosEvento.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace partyholic_api.Models
 {
     public partial class UsuariosEvento
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Username { get; set; }
         public int? CodGrupo { get; set; }
