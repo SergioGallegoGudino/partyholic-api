09ca01d65de3fa726333fff9219c6b266600764b
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-09-16 18:41:10 +0200

    Update UsuarioAuthDto.cs

--- Dto/UsuarioAuthDto.cs
+++ Dto/UsuarioAuthDto.cs
@@ -5,6 +5,7 @@
         public string Username { get; set; } = null!;
         public string? Email { get; set; }
         public string? Passwd { get; set; }
+        public string? FotoP { get; set; }
         public string? RolApp { get; set; }
     }
 }
