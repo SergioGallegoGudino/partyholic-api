4886950b1021264550c801d404b23f1267f3211b
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-09-16 18:56:53 +0200

    Update UsuariosController.cs

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -127,7 +127,7 @@ namespace partyholic_api.Controllers
                 user.Username = usuario.Username;
                 user.Email = usuario.Email;
                 user.Passwd = passHash;
-                user.FotoPerfil = usuario.FotoPerfil;
+                user.FotoP  = usuario.FotoPerfil;
                 user.RolApp = "user";
                 user.Nombre = usuario.Username;
                 user.Privacidad = "public";
