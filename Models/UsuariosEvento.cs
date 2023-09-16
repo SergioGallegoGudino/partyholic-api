d153b4816747f0932d40f23e4d37035c8e8becc5
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-09-16 17:14:57 +0200

    Update UsuariosEvento.cs

--- Models/UsuariosEvento.cs
+++ Models/UsuariosEvento.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace partyholic_api.Models
 {
     public partial class UsuariosEvento
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Username { get; set; }
